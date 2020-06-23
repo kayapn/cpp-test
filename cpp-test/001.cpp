35e08517d1a9e9ab0c03f6e1da81bd63ed83466e
kayapn <kayapn@yahoo.co.jp>
2020-06-23 14:45:44 +0000

    add 003

---
+++ cpp-test/001.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	cout << "Hello,world!" << endl;
+	cout << "Hello,world!" << endl;
+	cout << "Hello,world!" << endl;
+	cout << "Hello,world!" << endl;
+	cout << "Hello,world!" << endl;
+}
---
+++ cpp-test/003.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	cout << 1 + 1 << endl;
+	cout << 1 - 1 << endl;
+	cout << 1 * 1 << endl;
+	cout << 1 / 1 << endl;
+}
