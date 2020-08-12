85f61081d3c1dabc40c0636efab6e72d88c649fd
kayapn <kayapn@yahoo.co.jp>
2020-08-12 23:09:31 +0000

    20200813 ad

---
+++ rust-test/hello/target/debug/deps/hello-cd476b608df1bcd7.d
@@ -0,0 +1,5 @@
+/workspace/cpp-test/rust-test/hello/target/debug/deps/hello-cd476b608df1bcd7: src/main.rs
+
+/workspace/cpp-test/rust-test/hello/target/debug/deps/hello-cd476b608df1bcd7.d: src/main.rs
+
+src/main.rs:
---
+++ rust-test/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/workspace/cpp-test/rust-test/hello/target/debug/hello: /workspace/cpp-test/rust-test/hello/src/main.rs
